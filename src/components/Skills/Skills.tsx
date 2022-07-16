f487a869375163c262f9b1d8c07aa06d9f4ee612
Ruslan <ruslan.pershin98@gmail.com>
2022-07-16 11:47:40 +0300

    fix: main, skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import styles from './Skills.module.css'
 import styleContainer from '../../common/styles/Container.module.css'
+import sectionTitle from '../../common/styles/SectionTitle.module.css'
 import Skill from "./Skill/Skill";
 
 const Skills = () => {
     return (
         <section className={styles.skillsBlock}>
-            <div className={styleContainer.container + ' ' + styles.skillsContainer}>
-                <h2 className={styles.title}>My skills</h2>
-                <div className={styles.skills}>
-                    <Skill title={'Js'} description={'dasdasdasdasdasdas'}/>
-                    <Skill title={'CSS'} description={'dasdasdasdasdasdas'}/>
-                    <Skill title={'React'} description={'dasdasdasdasdasdas'}/>
+            <div className={styleContainer.container}>
+                <h2 className={sectionTitle.title}>Skills</h2>
+
+                <div className={styles.content}>
+                    <div className={styles.item}>
+                        <div className={styles.icon}></div>
+
+                        <div>
+                            <h3 className={styles.name}>HTML</h3>
+                            <span className={styles.description}>
+                                Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry's standard dummy text ever
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className={styles.item}>
+                        <div className={styles.icon}></div>
+
+                        <div>
+                            <h3 className={styles.name}>CSS</h3>
+                            <span className={styles.description}>
+                                Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry's standard dummy text ever
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className={styles.item}>
+                        <div className={styles.icon}></div>
+
+                        <div>
+                            <h3 className={styles.name}>JavaScript</h3>
+                            <span className={styles.description}>
+                                Lorem Ipsum is simply dummy text of the Lorem Ipsum has been the industry's standard dummy text ever
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
